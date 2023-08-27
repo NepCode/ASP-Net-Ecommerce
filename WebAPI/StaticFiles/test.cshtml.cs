5cb1c2d12256228d367119db45ec9d25c5cb4182
Antonio <aguilantonio16@gmail.com>
2023-08-27 14:53:24 -0600

    template email test array

---
+++ WebAPI/StaticFiles/test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAPI.StaticFiles
+{
+    public class test : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
